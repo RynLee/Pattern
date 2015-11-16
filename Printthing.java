e9e7f9d8caf4e6967ccf5ffe6915603c08c8e2cc
RynLee <ryanandrewlee@gmail.com>
2015-11-17 07:58:05 +0800

    Print pattern

---
+++ Printthing.java
@@ -0,0 +1,18 @@
+
+public class Printthing 
+{
+	public static void main(String[]args)
+	{
+		int i;
+		int n;
+		for (i=1; i<=5; i++)
+		{
+			
+			for (n=0; n<i; n++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
